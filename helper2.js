94f10847db88556770917cf7e6232d3f0d82ba0a
Eva S. Poulsen <eva@btech.au.dk>
2022-04-24 15:56:59 +0200

    module exports patterns

---
+++ helper2.js
@@ -0,0 +1,15 @@
+const user = function(data) {
+    return `${data} is logged in.`;
+};
+
+let id = function(id) {
+    return `${id}`;
+};
+
+let email = function(email) {
+    return email;
+};
+
+module.exports.user = user;
+module.exports.id = id;
+module.exports.email = email;
